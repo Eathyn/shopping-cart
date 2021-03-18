0efeed4ff27e1fb8cb803a2be559e989ccd5e283
Eathyn <eathynzakas@gmail.com>
2021-03-18 22:22:49 +0800

    feat: add cart module and product module

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import cartModule from '@/store/modules/cart';
+import productModule from '@/store/modules/product';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    cartModule,
+    productModule,
   },
 });
